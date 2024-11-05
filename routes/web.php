84493bf2fa310d40e9b39a0314a636c56662198f
hurulainytsaltsa <hurulainy01@gmail.com>
2024-11-05 13:26:42 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DetailsMakeUpController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MuaProfileController;
+use App\Http\Controllers\PackageMakeUpController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +32,8 @@ Route::resource('/ourprofile', MuaProfileController::class);
 Route::get('/details', function () {
     return view('layouts.detail_profile');
 });
+
+Route::get('/package', [PackageMakeUpController::class, 'index']);
+Route::resource('/package', PackageMakeUpController::class);
+
+Route::resource('/details_package', DetailsMakeUpController::class);
